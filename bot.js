bc944a2e2ad9fb57c1b4633adb0476b2388da023
SnakeAssassins <34872903+SnakeAssassins@users.noreply.github.com>
2017-12-27 19:53:07 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,6 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-// The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'your bot token here';
-
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
 client.on('ready', () => {
