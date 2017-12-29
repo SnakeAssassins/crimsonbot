2597a7f9485aa0914687bc5f04f9d5180859e126
SnakeAssassins <34872903+SnakeAssassins@users.noreply.github.com>
2017-12-29 00:04:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-//function for banning users
-function crimsonban(user,reason){
-    guild.ban(user, reason);
-    message.channel.send('BEGONE THOT');
-};
-
 //simple responses
 client.on('message', message => {
     if (message.content.includes('ping')) {
@@ -20,10 +14,10 @@ client.on('message', message => {
 });
 
  //user bans via commands  
- if (message.content === 'King Crimson, erase '){
+ /*if (message.content === 'King Crimson, erase '){
     if(message.member.roles.hasPermission("banMembers") ){
         crimsonban(message.mentions.members.first(),'decided by moderator');
     };
-};
+};*/
           
 client.login(process.env.BOT_TOKEN);
